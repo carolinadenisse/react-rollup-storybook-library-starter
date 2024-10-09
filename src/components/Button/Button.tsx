61fb1d370d0b457ddc587bfb4b9f32ca7f60e7eb
carolinadenisse <denisse.tobarc@gmail.com>
2024-10-09 16:43:59 -0300

    chore: added antdesign

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+import { Button as ButtonDesign} from 'antd';
 
 interface ButtonProps {
     label: string;
 }
 
 const Button = (props: ButtonProps) => {
-    return <button>{props.label}</button>
+    return <ButtonDesign type="primary">{props.label}</ButtonDesign>
 };
 
 export default Button;
